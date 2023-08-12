6bc2dead8a3e901221aa858af196314852305736
GwendiWisnuAnggoro <105532618+GwendiWisnuAnggoro@users.noreply.github.com>
2023-08-12 15:39:43 +0700

    ,

--- Leaderboard/sistemleaderboard.js
+++ Leaderboard/sistemleaderboard.js
@@ -4,69 +4,59 @@ let btn_switch = true;
 const task_box = document.querySelector(".task-box");
 const Max_Value = 5;
 
-for(let i = 0; i < Max_Value; i++){
-    new AmbilData(sheetID, "F", (data)=>{
-        let value_name = data[i].value;
-        new AmbilData(sheetID, "G", (data)=>{
-            let value_score = data[i].value;
-            task_box.innerHTML += `
-            <li class="task">
-                <h1>${i+1}</h1>
-                <p>${value_name}</p>
-                <p>${value_score} ♛</p>
-            </li>
-            `
-        })
-    })
-
+function tampilkanTask(nomor, nama, score) {
+    task_box.innerHTML += `
+        <li class="task">
+            <h1>${nomor}</h1>
+            <p>${nama}</p>
+            <p>${score} ♛</p>
+        </li>
+    `;
 }
 
-btn.addEventListener("click", ()=>{
-    if (btn_switch) {
-        task_box.innerHTML = ""
-        let code_name = "H";
-        let code_score = "I";
-        btn.textContent = "Tetris";
-        
-        for(let i = 0; i < Max_Value; i++){
-            new AmbilData(sheetID, code_name, (data)=>{
-                let value_name = data[i].value;
-                new AmbilData(sheetID, code_score, (data)=>{
-                    let value_score = data[i].value;
-                    task_box.innerHTML += `
-                    <li class="task">
-                        <h1>${i+1}</h1>
-                        <p>${value_name}</p>
-                        <p>${value_score} ♛</p>
-                    </li>
-                    `
-                })
-            })
+function fetchDataAndDisplay(codeName, codeScore, index) {
+    new AmbilData(sheetID, codeName, (dataName) => {
+        new AmbilData(sheetID, codeScore, (dataScore) => {
+            let value_name = dataName[index].value;
+            let value_score = dataScore[index].value;
+            tampilkanTask(index + 1, value_name, value_score);
+            if (task_box.querySelectorAll(".task").length === Max_Value) {
+                btn.removeAttribute("disabled");
+                btn.textContent = "Tetris"; // Ganti teks tombol
+                btn.classList.remove("disabled");
+            }
+        });
+    });
+}
 
-        }
-
-    } else {
-        task_box.innerHTML = ""
-        let code_name = "F",
-        code_score = "G"
-        btn.textContent = "Snake";
-        for(let i = 0; i < Max_Value; i++){
-            new AmbilData(sheetID, code_name, (data)=>{
-                let value_name = data[i].value;
-                new AmbilData(sheetID, code_score, (data)=>{
-                    let value_score = data[i].value;
-                    task_box.innerHTML += `
-                    <li class="task">
-                        <h1>${i+1}</h1>
-                        <p>${value_name}</p>
-                        <p>${value_score} ♛</p>
-                    </li>
-                    `
-                })
-            })
-
-        }
-        
+function updateDisplay(codeName, codeScore) {
+    task_box.innerHTML = "";
+    btn.setAttribute("disabled", "true"); // Menonaktifkan tombol
+    btn.textContent = "Loading..."; // Ganti teks tombol
+    btn.classList.add("disabled"); // Tambahkan kelas "disable"
+    for (let i = 0; i < Max_Value; i++) {
+        fetchDataAndDisplay(codeName, codeScore, i);
     }
-      btn_switch = !btn_switch;
-})
+}
+
+function switchToTetrisView() {
+    btn.textContent = "Tetris";
+    updateDisplay("H", "I");
+}
+
+function switchToSnakeView() {
+    btn.textContent = "Snake";
+    updateDisplay("F", "G");
+}
+
+btn.addEventListener("click", () => {
+    if (btn_switch) {
+        switchToTetrisView();
+    } else {
+        switchToSnakeView();
+    }
+    btn_switch = !btn_switch;
+});
+
+// Inisialisasi konten awal
+switchToSnakeView();
